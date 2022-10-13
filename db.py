d38af80405ed357db19ef32c0012dbbaeb7fcbee
Ryan Beckett <ryan.callum.beckett@gmail.com>
2022-10-13 09:40:23 +0100

    Db config changes

--- db.py
+++ db.py
@@ -1,8 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from configparser import ConfigParser
 
-SQLALCHEMY_DATABASE_URL = "postgresql://ryanbeckett:$Astonvillapark100@localhost/plantbased"
+config = ConfigParser()
+config.read('./config.cfg')
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{config['POSTGRES']['username']}:{config['POSTGRES']['password']}@{config['POSTGRES']['host']}/{config['POSTGRES']['database']}"
+
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
